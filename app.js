8e4f7c90ecbe5ddd5c033b25d87300e74a6cbeae
TUKAHEEBWARUHANGA IRAD <104001652+iroid2@users.noreply.github.com>
2023-03-24 11:15:56 +0300

    .

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ const swiper = new Swiper('#swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: [0, 0, -400],
+      },
+      next: {
+        translate: ["100%", 0, 0],
+      },
+    },
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
